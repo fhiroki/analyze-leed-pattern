040e66209beb9704ff9c289faf001a775597afd6
Hiroki Fujioka <hiroki976@gmail.com>
2019-04-25 12:59:45 +0900

    define detect_outer_circle method

--- scripts/main.py
+++ scripts/main.py
@@ -6,13 +6,10 @@ import matplotlib.pyplot as plt
 
 DATA_DIR = '../data/L16001-L17000/'
 OUT_DIR = '../output/image/'
+ismultiple = False
 
 
-def detecter(img, img_mask):
-    cimg = cv2.cvtColor(img_mask, cv2.COLOR_GRAY2RGB)
-    circles = cv2.HoughCircles(img_mask, cv2.HOUGH_GRADIENT, dp=1, minDist=90,
-                               param1=100, param2=6, minRadius=13, maxRadius=20)
-
+def detect_outer_circle(img):
     ret, img_thresh = cv2.threshold(img, 90, 90, cv2.THRESH_TOZERO)
     img_thresh = cv2.medianBlur(img_thresh, 9)
     img_thresh = cv2.adaptiveThreshold(img_thresh, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 101, 0)
@@ -20,37 +17,39 @@ def detecter(img, img_mask):
     outer_circle = cv2.HoughCircles(img_thresh, cv2.HOUGH_GRADIENT, dp=1, minDist=70,
                                     param1=140, param2=10, minRadius=440, maxRadius=460)
 
+    if outer_circle is not None:
+        x, y, r = np.around(outer_circle[0][0])
+        mask = img.copy()
+        cv2.circle(mask, (x, y), r, (0, 0, 0), -1)
+        # cv2.circle(img, (x, y), 10, (255, 0, 0), -1)  # draw center of circle
+        img -= mask
+
+    return img
+
+
+def detect_blob(img, img_mask):
+    cimg = cv2.cvtColor(img_mask, cv2.COLOR_GRAY2RGB)
+    circles = cv2.HoughCircles(img_mask, cv2.HOUGH_GRADIENT, dp=1, minDist=90,
+                               param1=100, param2=6, minRadius=13, maxRadius=20)
+
     circles = np.around(circles[0])
     for i in circles:
         cv2.circle(cimg, (i[0], i[1]), 10, (0, 0, 255), -1)
 
-    x = None
-    theta = None
-    if outer_circle is not None:
-        center = np.around(outer_circle[0][0])
-        cv2.circle(cimg, (center[0], center[1]), center[2], (0, 255, 0), 2)
-        cv2.circle(cimg, (center[0], center[1]), 10, (255, 0, 0), -1)
-
-        # for center in np.around(outer_circle[0]):
-        #     cv2.circle(cimg, (center[0], center[1]), center[2], (0, 255, 0), 2)
-        #     cv2.circle(cimg, (center[0], center[1]), 10, (255, 0, 0), -1)
-
-        vector = center - circles
-        x, theta = cv2.cartToPolar(vector[:, 0], vector[:, 1])
-        theta = np.pi - theta
-
-    return x, theta, cimg
+    return cimg
 
 
-def main(filename='L16501.tif'):
+def main(filename):
+    # img = cv2.imread(os.path.join(DATA_DIR, filename), -1)  # read image as original 16 bit
     img = cv2.imread(os.path.join(DATA_DIR, filename), cv2.IMREAD_GRAYSCALE)
     fig, axes = plt.subplots(2, 2)
     axes[0, 0].imshow(img)
-    axes[0, 0].set_title('original')
+    axes[0, 0].set_title(filename)
 
-    ret, img_mask = cv2.threshold(img, 100, 100, cv2.THRESH_TOZERO)
-    img_mask = cv2.medianBlur(img_mask, 9)
-    img_mask = cv2.adaptiveThreshold(img_mask, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 101, 0)
+    img = detect_outer_circle(img)
+    ret, img_mask = cv2.threshold(img, 95, 95, cv2.THRESH_TOZERO)
+    # img_mask = cv2.medianBlur(img_mask, 9)
+    img_mask = cv2.adaptiveThreshold(img_mask, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 101, 0)
     axes[0, 1].imshow(img_mask)
     axes[0, 1].set_title('adaptiveThreshold')
 
@@ -59,21 +58,27 @@ def main(filename='L16501.tif'):
     axes[1, 0].set_title('median blur')
 
     try:
-        x, theta, cimg = detecter(img, img_mask)
+        cimg = detect_blob(img, img_mask)
         # E = 51.1
         # r = 617.11
         # d = np.sqrt(150.4 / E) * np.sqrt(r ** 2 + x ** 2) / x
+
+        axes[1, 1].imshow(cimg)
+        axes[1, 1].set_title('detect circles')
     except:
         pass
 
-    axes[1, 1].imshow(cimg)
-    axes[1, 1].set_title('detect circles')
-    # plt.savefig(os.path.join(OUT_DIR, filename))
-    plt.show()
+    if ismultiple:
+        plt.savefig(os.path.join(OUT_DIR, filename))
+    else:
+        plt.show()
 
 
 if __name__ == "__main__":
-    main()
-    # for i in range(52):
-    #     filename = 'L16{}.tif'.format(450 + i)
-    #     main(filename)
+    if ismultiple:
+        for i in range(52):
+            filename = 'L16{}.tif'.format(450 + i)
+            main(filename)
+    else:
+        filename = 'L16501.tif'
+        main(filename)
