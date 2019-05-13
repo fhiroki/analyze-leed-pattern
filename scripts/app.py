f29ffd7150dab6d44bfe0a7865f46f27679cce89
Hiroki Fujioka <hiroki976@gmail.com>
2019-05-13 14:44:25 +0900

    build app template

--- scripts/app.py
+++ scripts/app.py
@@ -1,37 +1,39 @@
 import os
-import tkinter
-from tkinter import filedialog
-from tkinter import ttk
+import tkinter as tk
+from tkinter import Tk, Canvas, PanedWindow, Frame, Label, Button, Entry, Scrollbar
+from tkinter import filedialog, ttk
 
 import cv2
 from PIL import Image, ImageTk
 
 
-class Application(tkinter.Frame):
+class Application(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
         self.pack()
         self.create_widgets()
 
     def create_widgets(self):
-        pane = tkinter.PanedWindow(self.master, orient='horizontal')
-        pane.pack(expand=True, fill=tkinter.BOTH, side="left")
+        pane = PanedWindow(self.master, orient='vertical')
+        pane.pack(expand=True, fill=tk.BOTH, side='left')
 
-        self.frame = tkinter.Frame(pane, bd=2, relief="ridge")
+        self.frame = Frame(pane, bd=2, relief='ridge')
         pane.add(self.frame)
 
-        tkinter.Label(self.frame, text='dの理論値：').grid(column=0, row=0, padx=10, pady=10)
-        self.theoretical_d = tkinter.Entry(self.frame, width=15)
+        Label(self.frame, text='theoritical d of base:').grid(column=0, row=0, padx=10, pady=10)
+        self.theoretical_d = Entry(self.frame, width=15)
         self.theoretical_d.grid(column=1, row=0)
-        tkinter.Label(self.frame, text='Å').grid(column=2, row=0)
+        Label(self.frame, text='Å').grid(column=2, row=0)
 
-        tkinter.Label(self.frame, text='○基盤の画像').grid(column=0, row=1)
-        ttk.Button(self.frame, text='画像を選択', command=self.select_file).grid(column=0, row=2)
+        self.fm_base = Frame(pane, bd=2, relief='ridge', pady=10)
+        pane.add(self.fm_base)
+        Label(self.fm_base, text='○ images of base').grid(column=0, row=1)
+        ttk.Button(self.fm_base, text='select images', command=self.select_file).grid(column=0, row=2)
 
-        self.fm_img = tkinter.Frame(pane, bd=2, relief="ridge")
-        pane.add(self.fm_img)
-        # self.panel_img = tkinter.Label(self.fm_img)
-        # self.panel_img.pack()
+        self.fm_molecules = Frame(pane, bd=2, relief='ridge', pady=10)
+        pane.add(self.fm_molecules)
+        Label(self.fm_molecules, text='○ images of molecules').grid(column=0, row=1)
+        ttk.Button(self.fm_molecules, text='select images', command=self.select_file).grid(column=0, row=2)
 
     def get_d(self):
         print(self.theoretical_d.get())
@@ -40,15 +42,15 @@ class Application(tkinter.Frame):
         initialdir = os.path.abspath(os.path.dirname("__file__"))
         files = filedialog.askopenfilenames(initialdir=initialdir)
 
-        self.img_labels = []
+        self.img_canvas = []
         self.image = []
         for i in range(len(files)):
-            self.img_labels.append(tkinter.Label(self.fm_img))
+            self.img_canvas.append(Canvas(self.fm_img))
 
         for i, filename in enumerate(files):
             self.draw_image(filename)
-            self.img_labels[i].configure(image=self.image[i])
-            self.img_labels[i].pack()
+            self.img_canvas[i].create_image(0, 0, anchor='nw', image=self.image[i])
+            self.img_canvas[i].pack()
 
     def draw_image(self, path):
         img = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
@@ -60,9 +62,10 @@ class Application(tkinter.Frame):
 
 
 def main():
-    root = tkinter.Tk()
+    root = Tk()
     app = Application(master=root)
     app.master.geometry('700x800')
+    app.master.title('Analyze LEED pattern')
     app.mainloop()
 
 
