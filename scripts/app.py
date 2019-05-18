52fc86025c8ea7c651b3f014cf6c53ee0b2211b8
Hiroki Fujioka <hiroki976@gmail.com>
2019-05-18 11:34:00 +0900

    add input field of beam voltage

--- scripts/app.py
+++ scripts/app.py
@@ -13,6 +13,16 @@ class Application(tk.Frame):
         self.pack()
         self.create_widgets()
 
+        self.base_image_paths = []
+        self.base_images = []
+        self.base_img_canvas = []
+        self.base_voltages = []
+
+        self.mole_image_paths = []
+        self.mole_images = []
+        self.mole_img_canvas = []
+        self.mole_voltages = []
+
     def create_widgets(self):
         pane = PanedWindow(self.master, orient='vertical')
         pane.pack(expand=True, fill=tk.BOTH, side='left')
@@ -31,13 +41,13 @@ class Application(tk.Frame):
         pane.add(self.fm_base)
         Label(self.fm_base, text='○ Images of Base').grid(row=0, column=0)
         Label(self.fm_base, text='Images').grid(row=1, column=0, pady=100)
-        Label(self.fm_base, text='Base Voltage').grid(row=2, column=0, pady=10)
+        Label(self.fm_base, text='Base Voltage[V]').grid(row=2, column=0, pady=20)
 
         self.fm_base_canvas = Canvas(self.fm_base, bd=2, relief='ridge')
         self.fm_base_canvas.grid(row=1, column=1, rowspan=2, sticky='W' + 'E' + 'N' + 'S', padx=35)
 
         self.fm_base_btn = ttk.Button(self.fm_base_canvas, text='select images')
-        self.fm_base_btn.config(command=lambda: self.select_file(True))
+        self.fm_base_btn.config(command=lambda: self.select_file(isBase=True))
         self.fm_base_btn.grid(row=0, column=0, padx=500, pady=120)
 
         # Images of Molecules frame
@@ -45,21 +55,27 @@ class Application(tk.Frame):
         pane.add(self.fm_mole)
         Label(self.fm_mole, text='○ Images of Molecules').grid(row=0, column=0)
         Label(self.fm_mole, text='Images').grid(row=1, column=0, pady=100)
-        Label(self.fm_mole, text='Base Voltage').grid(row=2, column=0, pady=10)
+        Label(self.fm_mole, text='Base Voltage[V]').grid(row=2, column=0, pady=20)
 
         self.fm_mole_canvas = Canvas(self.fm_mole, bd=2, relief='ridge')
         self.fm_mole_canvas.grid(row=1, column=1, rowspan=2, sticky='W' + 'E' + 'N' + 'S')
 
         self.fm_mole_btn = ttk.Button(self.fm_mole_canvas, text='select images')
-        self.fm_mole_btn.config(command=lambda: self.select_file(False))
+        self.fm_mole_btn.config(command=lambda: self.select_file(isBase=False))
         self.fm_mole_btn.grid(row=0, column=0, padx=500, pady=120)
 
+        # Run blob detection
         self.fm_run = Frame(pane, bd=2, relief='ridge', pady=10)
         pane.add(self.fm_run)
         ttk.Button(self.fm_run, text='RUN', command=self.run).pack()
 
     def run(self):
-        pass
+        print('Base')
+        print('image: {}'.format(self.base_image_paths))
+        print('voltages: {}\n'.format([base_voltage.get() for base_voltage in self.base_voltages]))
+        print('Molecules')
+        print('image: {}'.format(self.mole_image_paths))
+        print('voltages: {}'.format([mole_voltage.get() for mole_voltage in self.mole_voltages]))
 
     def get_d(self):
         print(self.theoretical_d.get())
@@ -70,31 +86,33 @@ class Application(tk.Frame):
         files = filedialog.askopenfilenames(initialdir=initialdir)
 
         if isBase:
-            self.base_images = []
-            self.base_img_canvas = []
             for i in range(len(files)):
                 self.base_img_canvas.append(Canvas(self.fm_base_canvas))
+                self.base_voltages.append(Entry(self.fm_base_canvas))
 
             for i, filename in enumerate(files):
-                self.read_image(filename, isBase)
+                self.base_image_paths.append(filename)
+                self.base_images.append(self.read_image(filename))
                 self.base_img_canvas[i].create_image(self.base_images[i]['width'] / 2,
                                                      self.base_images[i]['height'] / 2,
                                                      image=self.base_images[i]['img'])
-                self.base_img_canvas[i].grid(row=0, column=i, padx=10, pady=10)
+                self.base_img_canvas[i].grid(row=0, column=i, padx=15, pady=10)
+                self.base_voltages[i].grid(row=1, column=i, pady=20)
 
             self.fm_base_btn.grid_forget()
         else:
-            self.mole_images = []
-            self.mole_img_canvas = []
             for i in range(len(files)):
                 self.mole_img_canvas.append(Canvas(self.fm_mole_canvas))
+                self.mole_voltages.append(Entry(self.fm_mole_canvas))
 
             for i, filename in enumerate(files):
-                self.read_image(filename, isBase)
+                self.mole_image_paths.append(filename)
+                self.mole_images.append(self.read_image(filename))
                 self.mole_img_canvas[i].create_image(self.mole_images[i]['width'] / 2,
                                                      self.mole_images[i]['height'] / 2,
                                                      image=self.mole_images[i]['img'])
-                self.mole_img_canvas[i].grid(row=0, column=i, padx=10, pady=10)
+                self.mole_img_canvas[i].grid(row=0, column=i, padx=15, pady=10)
+                self.mole_voltages[i].grid(row=1, column=i, pady=20)
 
             self.fm_mole_btn.grid_forget()
 
@@ -105,7 +123,7 @@ class Application(tk.Frame):
         # self.fm_base_canvas.config(xscrollcommand=hscrollbar.set)
         # hscrollbar.grid(row=2, column=1, sticky='E' + 'W')
 
-    def read_image(self, path, isBase):
+    def read_image(self, path):
         img = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
         height = int(img.shape[0] / 5)
         width = int(img.shape[1] / 5)
@@ -113,10 +131,7 @@ class Application(tk.Frame):
         img = Image.fromarray(img)
         img = ImageTk.PhotoImage(img)
 
-        if isBase:
-            self.base_images.append({'img': img, 'height': height, 'width': width})
-        else:
-            self.mole_images.append({'img': img, 'height': height, 'width': width})
+        return {'img': img, 'height': height, 'width': width}
 
 
 def main():
