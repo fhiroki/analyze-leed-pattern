98f91f814b28f3b45393de5c9c49a6540242d5be
Hiroki Fujioka <hiroki976@gmail.com>
2019-09-06 15:16:10 +0900

    #12 fix test

--- tests/test_detector.py
+++ tests/test_detector.py
@@ -2,10 +2,14 @@ import unittest
 from scripts.detector import detect_base_blob
 
 
+DELTA = 10
+
+
 class TestDetectBaseBlob(unittest.TestCase):
     '''
     test case of detector.py
     '''
+
     def test_Au110(self):
         ''' Test of 6P/Au(110) '''
         DATA_DIR = 'data/6P_Au110/image/Au110'
@@ -14,7 +18,7 @@ class TestDetectBaseBlob(unittest.TestCase):
         actual_r = detect_base_blob(DATA_DIR, base_type,
                                     [134.4, 123.1, 115.7, 83.8, 79.9, 68.8, 63.4, 58.9, 57.0, 37.8, 29.5],
                                     isfilename=True, isplot=False, manual_r=expected_r)
-        self.assertAlmostEqual(expected_r, actual_r, delta=10)
+        self.assertAlmostEqual(expected_r, actual_r, delta=DELTA)
 
     def test_Au111(self):
         ''' Test of BrHPB/Au(111) '''
@@ -24,7 +28,7 @@ class TestDetectBaseBlob(unittest.TestCase):
         actual_r = detect_base_blob(DATA_DIR, base_type,
                                     [79.9, 95.9, 121.4, 163.8, 189.8, 256.1, 297.6, 346.9],
                                     isfilename=True, isplot=False, manual_r=expected_r)
-        self.assertAlmostEqual(expected_r, actual_r, delta=10)
+        self.assertAlmostEqual(expected_r, actual_r, delta=DELTA)
 
     def test_Ag110(self):
         ''' Test of Coronene/Ag(110) '''
@@ -34,7 +38,7 @@ class TestDetectBaseBlob(unittest.TestCase):
         actual_r = detect_base_blob(DATA_DIR, base_type,
                                     [33.1, 35.4, 38.1, 41.4, 50.8, 56.9, 60.1, 65.6],
                                     isfilename=True, isplot=False, manual_r=expected_r)
-        self.assertAlmostEqual(expected_r, actual_r, delta=10)
+        self.assertAlmostEqual(expected_r, actual_r, delta=DELTA)
 
     def test_Ag111(self):
         ''' Test of Coronene/Ag(111) '''
@@ -44,11 +48,8 @@ class TestDetectBaseBlob(unittest.TestCase):
         actual_r = detect_base_blob(DATA_DIR, base_type,
                                     [80.6, 94.7, 109.2, 122.9, 136.0, 150.9, 159.1,
                                      179.2, 193.7, 215.3, 230.3, 252.7, 264.9],
-                                    image_paths=['L16469.tif', 'L16470.tif', 'L16471.tif', 'L16472.tif', 'L16473.tif',
-                                                 'L16474.tif', 'L16475.tif', 'L16476.tif', 'L16477.tif', 'L16478.tif',
-                                                 'L16479.tif', 'L16480.tif', 'L16481.tif'],
                                     isfilename=True, isplot=False, manual_r=expected_r)
-        self.assertAlmostEqual(expected_r, actual_r, delta=10)
+        self.assertAlmostEqual(expected_r, actual_r, delta=DELTA)
 
     def test_Cu110(self):
         ''' Test of BV/Cu(110) '''
@@ -58,7 +59,7 @@ class TestDetectBaseBlob(unittest.TestCase):
         actual_r = detect_base_blob(DATA_DIR, base_type,
                                     [163.5, 131.1, 61.4, 139.8, 136.1, 118.1, 60.8, 154.2, 136.3, 121.2, 63.4],
                                     isfilename=True, isplot=False, manual_r=expected_r)
-        self.assertAlmostEqual(expected_r, actual_r, delta=10)
+        self.assertAlmostEqual(expected_r, actual_r, delta=DELTA)
 
     def test_Cu111(self):
         ''' Test of Coronene/Cu(111) '''
@@ -67,10 +68,8 @@ class TestDetectBaseBlob(unittest.TestCase):
         expected_r = 603.2
         actual_r = detect_base_blob(DATA_DIR, base_type,
                                     [98.9, 131.6, 141.1, 177.9, 231.6, 321.2],
-                                    image_paths=['L4898.tif', 'L4899.tif', 'L4900.tif',
-                                                 'L4901.tif', 'L4902.tif', 'L4903.tif'],
                                     isfilename=True, isplot=False, manual_r=expected_r)
-        self.assertAlmostEqual(expected_r, actual_r, delta=10)
+        self.assertAlmostEqual(expected_r, actual_r, delta=DELTA)
 
 
 if __name__ == "__main__":
