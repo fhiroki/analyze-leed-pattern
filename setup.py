ee49b42858b1efad774eac378768b90eb63339d1
Hiroki Fujioka <hiroki976@gmail.com>
2020-02-06 12:28:55 +0900

    install requirements.txt

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
-    # install_requires=reqs,
+    install_requires=reqs,
     entry_points='''
         [console_scripts]
         {app}=leed.{pkg}:main
